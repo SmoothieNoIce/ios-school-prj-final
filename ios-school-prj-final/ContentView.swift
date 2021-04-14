4b9e595bfc76d7355813ac63604bb9de10d748c9
NTOU <fus22803281@gmail.com>
2021-04-14 15:25:51 +0800

    Initial Commit

---
+++ ios-school-prj-final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-school-prj-final
+//
+//  Created by User17 on 2021/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-school-prj-final/ios_school_prj_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_school_prj_finalApp.swift
+//  ios-school-prj-final
+//
+//  Created by User17 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct ios_school_prj_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
